3f9657602de5a1a075077db8f57a9dd07c68cfac
Raphael <raphaelverchain@gmail.com>
2025-05-28 04:19:46 +0200

    deploiement

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,14 +44,28 @@ export default function RootLayout({
         `}} />
 
         {children}
-        
+
         {/* Google Tag Manager - Script */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-54WGQRVC');`}
+        </Script>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P1BCX9K325"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P1BCX9K325');
+          `}
         </Script>
       </body>
     </html>
